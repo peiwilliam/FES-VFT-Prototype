ca9bc7d62b8155e23a4dc5a7753eae2c2165af76
peiwilliam <58003316+peiwilliam@users.noreply.github.com>
2021-05-09 13:36:35 -0400

    begun arduino connection code

---
+++ Assets/Scripts/Serial Connection Scripts/ConnectionManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO.Ports;
+using UnityEngine;
+
+public class ConnectionManager : MonoBehaviour
+{
+    private SerialPort _stream;
+
+    private void Start()
+    {
+        _stream = new SerialPort("COM4", 9600);
+        _stream.ReadTimeout = 50;
+        _stream.Open();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public string ReadFromArduino(int timeout = 0)
+    {
+        _stream.ReadTimeout = timeout;
+
+        try
+        {
+            return _stream.ReadLine();
+        }
+        catch (TimeoutException exception)
+        {
+            Debug.Log(exception.Message);
+            return null;
+        }
+    }
+}
